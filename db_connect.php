054a095ee42ecc6c150d02172e8c004b8f953a74
Ace Lake <96988100+AceLake@users.noreply.github.com>
2023-11-17 07:44:18 -0700

    jot the jokes part 1 up

---
+++ db_connect.php
@@ -0,0 +1,20 @@
+<?php
+ini_set('display_errors', '1');
+ini_set('display_startup_errors', '1');
+error_reporting(E_ALL);
+
+// modify these settings according to the account on your database server.
+$host = "localhost";
+$port = "8889";
+$username = "root";
+$user_pass = "root";
+$database_in_use = "jokespart1";
+
+
+$mysqli = mysqli_connect($host, $username, $user_pass, $database_in_use, $port);
+if (!$mysqli) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+?>
